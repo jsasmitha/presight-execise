3f2f8941f4df99f11c013fc1a79c5d5d5a924e1a
Asmitha Janardhana <asmitha@Asmithas-Air.lan>
2026-08-23 18:21:33 +0400

    Changes to docker related changes

---
+++ server/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+DB_FILE="./database/users.db"
+
+if [ ! -f "$DB_FILE" ]; then
+    echo "Database file not found. Creating and seeding SQLite database..."
+   yarn seed
+else
+    echo "Database file already exists. Skipping creation."
+fi
+
+echo "Starting the server..."
+yarn start
